d90149e6647487351a48ba96b7b56dfcf6f0e7b5
zkcm <zkcm@zkcmdeMacBook-Pro.local>
2016-12-27 17:56:21 +0800

    自定义底图的配置

--- Map2history/app/src/main/java/com/haonight/www/map2history/MainActivity.java
+++ Map2history/app/src/main/java/com/haonight/www/map2history/MainActivity.java
@@ -1,13 +1,124 @@
 package com.haonight.www.map2history;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends Activity {
+
+
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        MapView.setMapCustomEnable(true);
+        setMapCustomFile(this);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        initMap();
+    }
+
+    private void setMapCustomFile(Context context) {
+        FileOutputStream out = null;
+        InputStream inputStream = null;
+        String moduleName = null;
+        try {
+            inputStream = context.getAssets()
+                    .open("customConfigdir/custom_config.txt");
+            byte[] b = new byte[inputStream.available()];
+            inputStream.read(b);
+
+            moduleName = context.getFilesDir().getAbsolutePath();
+            File f = new File(moduleName + "/" + "custom_config.txt");
+            if (f.exists()) {
+                f.delete();
+            }
+            f.createNewFile();
+            out = new FileOutputStream(f);
+            out.write(b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        MapView.setCustomMapStylePath(moduleName + "/custom_config.txt");
+    }
+
+    private void initMap() {
+        BaiduMap mBaiduMap = mapView.getMap();
+
+        //普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+//        //卫星地图
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+//
+//        //空白地图, 基础地图瓦片将不会被渲染。在地图类型中设置为NONE，将不会使用流量下载基础地图瓦片图层。使用场景：与瓦片图层一起使用，节省流量，提升自定义瓦片图下载速度。
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NONE);
+
+
+    }
+
+    private void initView() {
+        mapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+
+    private byte[] InputStreamToByte(InputStream is) throws IOException {
+        ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+        int ch;
+        while ((ch = is.read()) != -1) {
+            bytestream.write(ch);
+        }
+        byte imgdata[] = bytestream.toByteArray();
+        bytestream.close();
+        return imgdata;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mapView.onPause();
     }
 }
--- Map2history/app/src/main/java/com/haonight/www/map2history/app/MyApplication.java
+++ Map2history/app/src/main/java/com/haonight/www/map2history/app/MyApplication.java
@@ -2,9 +2,24 @@ package com.haonight.www.map2history.app;
 
 import android.app.Application;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 /**
  * Created by SHANGLIKUN on 16/12/27.
  */
 
 public class MyApplication extends Application {
+
+    MapView mapView;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //map init
+        SDKInitializer.initialize(getApplicationContext());
+
+
+    }
 }
