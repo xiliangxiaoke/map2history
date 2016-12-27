043de1348cdba40bdc9c666a710d06329ca71952
zkcm <zkcm@zkcmdeMacBook-Pro.local>
2016-12-27 16:22:31 +0800

    ssss

---
+++ Map2history/app/src/main/java/com/haonight/www/map2history/app/MyApplication.java
@@ -0,0 +1,10 @@
+package com.haonight.www.map2history.app;
+
+import android.app.Application;
+
+/**
+ * Created by SHANGLIKUN on 16/12/27.
+ */
+
+public class MyApplication extends Application {
+}
---
+++ Map2history/app/src/main/java/com/haonight/www/map2history/basec/MyAppCompatActivity.java
@@ -0,0 +1,10 @@
+package com.haonight.www.map2history.basec;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SHANGLIKUN on 16/12/27.
+ */
+
+public class MyAppCompatActivity extends AppCompatActivity {
+}
